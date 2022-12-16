f02b6c0448ca04f86ce92e488c072fdf2ec57557
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2022-12-16 12:47:40 -0400

    create simple snake game with bevy

---
+++ src/arena.rs
@@ -0,0 +1,91 @@
+use bevy::prelude::*;
+use rand::prelude::random;
+
+use crate::snake::{self, SnakeSegment, SnakeSegments};
+
+pub const ARENA_WIDTH: u32 = 10;
+pub const ARENA_HEIGHT: u32 = 10;
+const FOOD_COLOR: Color = Color::rgb(1.0, 0.0, 1.0);
+
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Component)]
+pub struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
+pub fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+pub fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
+}
+
+#[derive(Component)]
+pub struct Food;
+
+pub fn food_spawner(mut commands: Commands) {
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: FOOD_COLOR,
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Food)
+        .insert(Position {
+            x: (random::<f32>() * ARENA_WIDTH as f32) as i32,
+            y: (random::<f32>() * ARENA_HEIGHT as f32) as i32,
+        })
+        .insert(Size::square(0.8));
+}
+
+pub struct GameOverEvent;
+
+pub fn game_over(
+    mut commands: Commands,
+    mut reader: EventReader<GameOverEvent>,
+    segments_res: ResMut<SnakeSegments>,
+    food: Query<Entity, With<Food>>,
+    segments: Query<Entity, With<SnakeSegment>>,
+) {
+    if reader.iter().next().is_some() {
+        for ent in food.iter().chain(segments.iter()) {
+            commands.entity(ent).despawn();
+        }
+        snake::spawn_snake(commands, segments_res);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::{prelude::*, time::FixedTimestep};
+
+mod arena;
+mod camera;
+mod snake;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "I am a window!".to_string(),
+                width: 500.,
+                height: 500.,
+                ..default()
+            },
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(snake::SnakeSegments::default())
+        .insert_resource(snake::LastTailPosition::default())
+        .add_event::<snake::GrowthEvent>()
+        .add_event::<arena::GameOverEvent>()
+        .add_startup_system(camera::setup_camera)
+        .add_startup_system(snake::spawn_snake)
+        .add_system(snake::snake_movement_input.before(snake::snake_movement))
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(arena::position_translation)
+                .with_system(arena::size_scaling),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(arena::food_spawner),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(0.150))
+                .with_system(snake::snake_movement)
+                .with_system(snake::snake_eating.after(snake::snake_movement))
+                .with_system(snake::snake_growth.after(snake::snake_eating)),
+        )
+        .add_system(arena::game_over.after(snake::snake_movement))
+        .run();
+}
